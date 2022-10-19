2601063df4d33f953506c0d4135b6119ce4b97a3
qblassk <g.bulaschevich@gmail.com>
2022-10-19 19:31:33 -0300

    registro

--- index.js
+++ index.js
@@ -6,10 +6,25 @@ const app = express();
 const publicFolderPath = path.resolve('public');
 app.use(express.static(publicFolderPath));
 
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
 const PORT = process.env.PORT || 3000;
-app.listen(port, () => 
+app.listen(PORT, () => 
    console.log('Servidor funcionando en puerto ' + PORT));
 
 app.get('/', (req, res) => {
    res.sendFile(path.resolve('views/home.html'));
 });
+
+app.get('/register.html', (req, res) => {
+   res.sendFile(path.resolve('views/register.html'));
+});
+
+app.get('/login.html', (req, res) => {
+   res.sendFile(path.resolve('views/register.html'));
+});
+
+app.post('/', (req, res) => {
+   return res.send(req.body);
+})
