94b386dce87fea0e1a847a04bdc9ef4c19a56347
qblassk <g.bulaschevich@gmail.com>
2022-10-19 16:16:17 -0300

    cambios package.json

--- app.js
+++ app.js
@@ -6,10 +6,9 @@ const app = express();
 const publicFolderPath = path.resolve('public');
 app.use(express.static(publicFolderPath));
 
-const APP_PORT = 3000;
-app.listen(APP_PORT, () => {
-   console.log('Servidor funcionando en puerto ' + APP_PORT);
-});
+const PORT = process.env.PORT || 3000;
+app.listen(port, () => 
+   console.log('Servidor funcionando en puerto ' + PORT));
 
 app.get('/', (req, res) => {
    res.sendFile(path.resolve('views/home.html'));
